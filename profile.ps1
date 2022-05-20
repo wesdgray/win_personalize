e6546b4ea8ff546c9288e280dd21f4daa085983d
Wes Gray <wesgray@gmail.com>
2022-05-19 18:52:37 -0700

    Add winget apps, fix powershell profile for new oh-my-posh version

--- profile.ps1
+++ profile.ps1
@@ -2,4 +2,4 @@ Set-PSReadLineOption -EditMode Vi -ViModeIndicator Cursor
 Set-PSReadLineKeyHandler -ViMode Insert -Chord 'Ctrl+Spacebar' -Function MenuComplete
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-Theme Darkblood
+Set-PoshPrompt -Theme Darkblood
